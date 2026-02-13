3fe337f7cb85a44e8b2d2a31732f596f170f1ab1
Aritri Baidya <aritribaidya1111@gmail.com>
2026-02-13 17:21:12 +0530

    Add files via upload

---
+++ knowledge_graphs/kg_builder.py
@@ -0,0 +1,26 @@
+from knowledge_graphs.neo4j_client import  run_query
+
+def build_kg(state):
+    entities = state["entities"]
+
+    for item in entities:
+        for d in item["drugs"]:
+            for g in item["genes"]:
+                for ds in item["diseases"]:
+
+                    q = """
+                    MERGE (dr:Drug {name:$d})
+                    MERGE (g:Gene {name:$g})
+                    MERGE (di:Disease {name:$di})
+
+                    MERGE (dr)-[:TARGETS]->(g)
+                    MERGE (g)-[:ASSOCIATED_WITH]->(di)
+                    """
+
+                    run_query(q, {
+                        "d": d,
+                        "g": g,
+                        "di": ds
+                    })
+
+    return {"kg_done": True}
